8da36048ac3a7513b8bc4692a015ea52fc8fa534
jaedeok_dev <jaedeok_dev@jake0208-PC.local>
2023-04-24 13:58:59 +0900

    test:

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -1,5 +1,8 @@
-#!/bin/bash
-source ~/.bashrc
+#!/bin/bash -c
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 cd /home/ubuntu/sprint-practice-deploy-for04
 npm install
 npm install pm2@latest -g
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-source ~/.bashrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 cd /home/ubuntu/sprint-practice-deploy-for04
 pm2 stop app.js 2> /dev/null || true
 pm2 delete app.js 2> /dev/null || true
