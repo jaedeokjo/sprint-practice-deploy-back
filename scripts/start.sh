5f12b51302ee94c7ece565f57677b7cb36746ee6
jaedeok_dev <jaedeok_dev@jake0208-PC.local>
2023-04-24 15:45:09 +0900

    test:

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 cd /home/ubuntu/sprint-practice-deploy-for04
-node --version
-npm --version
-npm ls -g
-pm2 start app.js
+authbind --deep pm2 start app.js
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -2,4 +2,3 @@
 cd /home/ubuntu/sprint-practice-deploy-for04
 pm2 stop app.js 2> /dev/null || true
 pm2 delete app.js 2> /dev/null || true
-
