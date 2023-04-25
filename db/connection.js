48fc6e6b3e7052e1e5e28d0856d87ea44e89324f
jaedeok_dev <jaedeok_dev@jake0208-PC.local>
2023-04-25 12:12:08 +0900

    fix: Run server even if db isn't connected

--- db/connection.js
+++ db/connection.js
@@ -2,17 +2,23 @@ const mysql = require('mysql2');
 const dotenv = require('dotenv');
 dotenv.config();
 
+const host = process.env.DATABASE_HOST
+const user = process.env.DATABASE_USER
+const password = process.env.DATABASE_PASSWORD
+const port = process.env.DATABASE_PORT
+
 const con = mysql.createConnection({
-  host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  port: process.env.DATABASE_PORT
+  host, user, password, port
 });
 
 con.connect((err) => {
-  if (err) {
-    console.log(err)
-    con.end();
+  try {
+    if (err) {
+      console.log(err)
+      con.end();
+    }
+  } catch {
+    console.log(`Can't contact db ${host}, ${user}, ${password}, ${port}`)
   }
 });
 
