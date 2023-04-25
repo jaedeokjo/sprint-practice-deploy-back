c20953b2420032bdf6875afab857a22ae16a0c81
jaedeok_dev <jaedeok_dev@jake0208-PC.local>
2023-04-25 15:40:13 +0900

    test: Add env

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 cd /home/ubuntu/sprint-practice-deploy-for04
+export DATABASE_USER=$(aws ssm get-parameters --region us-east-2 --names DATABASE_USER --query Parameters[0].Value | sed 's/"//g')
+export DATABASE_PASSWORD=$(aws ssm get-parameters --region us-east-2 --names DATABASE_PASSWORD --query Parameters[0].Value | sed 's/"//g')
+export DATABASE_PORT=$(aws ssm get-parameters --region us-east-2 --names DATABASE_PORT --query Parameters[0].Value | sed 's/"//g')
+export DATABASE_HOST=$(aws ssm get-parameters --region us-east-2 --names DATABASE_HOST --query Parameters[0].Value | sed 's/"//g')
 /bin/bash -lc "pm2 start app.js"
